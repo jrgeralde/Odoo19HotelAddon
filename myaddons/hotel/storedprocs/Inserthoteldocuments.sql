3a8a6341a5b13d9fbff3ecdd710d104a63c62739
John Roy Geralde <95401047+jrgeralde@users.noreply.github.com>
2026-04-14 17:12:03 +0800

    Add files via upload

---
+++ myaddons/hotel/storedprocs/Inserthoteldocuments.sql
@@ -0,0 +1,7 @@
+INSERT INTO hotel_documents (name, description)
+VALUES 
+    ('GRC', 'Guest Registration Card'),
+    ('RC', 'Room Charge'),
+    ('PR', 'Payment Receipt'),
+    ('DM', 'Debit Memo'),
+    ('CM', 'Credit Memo');
